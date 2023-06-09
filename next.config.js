5d14620e5aff9894f78cbcc34cef949768fb8d93
SoloM <93363580+SoloMReal@users.noreply.github.com>
2023-06-08 21:45:57 -0700

    First commit

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    newNextLinkBehavior: false,
+  },
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:5328/api/:path*'
+            : '/api/',
+      },
+      
+    ]
+  },
+}
+
+module.exports = nextConfig
